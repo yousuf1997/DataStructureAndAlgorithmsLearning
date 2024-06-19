48376f63caf1fad17ad2c40fc62919e501c6cefe
Mohamed Abdul <mohamedyousufmo@gmail.com>
2024-06-19 14:51:10 -0700

    heap implementation

--- DataStructuresAndAlgorithms/src/com/datastructuresandalgorithms/Main.java
+++ DataStructuresAndAlgorithms/src/com/datastructuresandalgorithms/Main.java
@@ -1,24 +1,28 @@
 package com.datastructuresandalgorithms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         MaxHeap<Integer> maxHeap = new MaxHeap<>();
 
-        maxHeap.add(10);
-        maxHeap.add(7);
-        maxHeap.add(9);
-        maxHeap.add(4);
-        maxHeap.add(7);
-        maxHeap.add(5);
-        maxHeap.add(11);
-        maxHeap.add(2);
-        maxHeap.add(1);
-        maxHeap.add(6);
 
-        System.out.println(maxHeap.getMax());
+        List<Integer> mutableList = new ArrayList<>();
 
-        maxHeap.replace(25, 5);
+        // Adding 13 numbers to the list
+        mutableList.add(1);
+        mutableList.add(6);
+        mutableList.add(9);
+        mutableList.add(2);
+        mutableList.add(7);
+        mutableList.add(5);
+        mutableList.add(7);
+        mutableList.add(4);
+        mutableList.add(10);
 
-        System.out.println(maxHeap.getMax());
+        maxHeap.heapSort(mutableList);
+
+        mutableList.forEach(integer -> System.out.println(integer));
     }
 }
--- DataStructuresAndAlgorithms/src/com/datastructuresandalgorithms/MaxHeap.java
+++ DataStructuresAndAlgorithms/src/com/datastructuresandalgorithms/MaxHeap.java
@@ -16,12 +16,53 @@ public class MaxHeap<T extends Comparable<T>> {
             // we added root so we do not need to percolate up
             return;
         }
-        percolate(container.size() - 1);
+        percolate(container.size() - 1, container);
     }
 
     public T getMax() {
-        if (container.isEmpty()) return null;
+        if (container.size() == 0) return null;
         return container.get(0);
+    }
+
+    public boolean isEmpty() {
+        return container.isEmpty();
+    }
+
+    public void heapSort(List<T> elements) {
+        // assume that the first element is the placeholder
+        makeHeap(elements);
+        int index = elements.size() - 1;
+        while(index > -1) {
+            T topElement = elements.get(0);
+
+            // swap with the current element
+            elements.set(0, elements.get(index));
+            elements.set(index, topElement);
+
+            siftDown(0, elements, index - 1);
+            index--;
+        }
+    }
+
+    private void makeHeap(List<T> elements) {
+        // assume that the first element is the placeholder
+        int index = elements.size() / 2;
+        while (index >= 0) {
+            siftDown(index, elements);
+            index--;
+        }
+    }
+
+    public void removeMax() {
+        if (container.size() == 0) {
+            // nothing to remove;
+            return;
+        }
+        container.set(0, container.get(container.size() - 1));
+        // remove the last element
+        container.remove(container.size() - 1);
+        // sift down
+        siftDown(0, container);
     }
 
     public void replace(T element, int position) {
@@ -31,9 +72,9 @@ public class MaxHeap<T extends Comparable<T>> {
         T currentValue = container.get(position);
         container.set(position, element);
         if (currentValue.compareTo(element) > 0) {
-            siftDown(position);
+            siftDown(position, container);
         } else {
-            percolate(position);
+            percolate(position, container);
         }
     }
 
@@ -42,13 +83,12 @@ public class MaxHeap<T extends Comparable<T>> {
      * heap property
      * @param fromPosition
      */
-    private void percolate(int fromPosition) {
-        boolean heapPropertyMet = false;
-        while(!heapPropertyMet) {
-            int parentPosition = (fromPosition / 2);
+    private void percolate(int fromPosition, List<T> container) {
+        while(true) {
+            int parentPosition = getParent(fromPosition);
             if (fromPosition == 0 || container.get(parentPosition).compareTo(container.get(fromPosition)) >= 0) {
                 // heap property condition met
-                heapPropertyMet = true;
+                break;
             } else {
                 // swap
                 T temp = container.get(parentPosition);
@@ -65,33 +105,59 @@ public class MaxHeap<T extends Comparable<T>> {
      * heap property
      * @param fromPosition
      */
-    private void siftDown(int fromPosition) {
+    private void siftDown(int fromPosition, List<T> container, int ...newSize) {
        int sizeOfContainer = container.size();
+
+       if (newSize.length > 0) {
+           sizeOfContainer = newSize[0];
+       }
+
        while (true) {
+
+           int leftChildIndex = getLeftChild(fromPosition);
+           int rightChildIndex = getRightChild(fromPosition);
+
            int positionToBeSwapped = -1;
            // check the bounds of the left and right child
-           if (2 * fromPosition < sizeOfContainer && (2 * fromPosition) + 1 < sizeOfContainer) {
+           if (leftChildIndex < sizeOfContainer && rightChildIndex < sizeOfContainer) {
                // check which one is bigger
-               if (container.get(2 * fromPosition).compareTo(container.get((2 * fromPosition) + 1 )) > 0) {
+               if (container.get(leftChildIndex).compareTo(container.get(rightChildIndex)) >= 0) {
                    // left child is bigger
-                   positionToBeSwapped = 2 * fromPosition;
+                   positionToBeSwapped = leftChildIndex;
                } else {
                    // right child is bigger
-                   positionToBeSwapped = (2 * fromPosition) + 1;
+                   positionToBeSwapped = rightChildIndex;
                }
-           } else if (2 * fromPosition < sizeOfContainer && (2 * fromPosition) + 1 >= sizeOfContainer) {
-               positionToBeSwapped = 2 * fromPosition;
-           } else if (2 * fromPosition >= sizeOfContainer && (2 * fromPosition) + 1 < sizeOfContainer){
-               positionToBeSwapped = (2 * fromPosition) + 1;
+           } else if (leftChildIndex < sizeOfContainer) {
+               positionToBeSwapped = leftChildIndex;
+           } else if (rightChildIndex < sizeOfContainer){
+               positionToBeSwapped = rightChildIndex;
            } else {
-               // heapproperty achieved
+               // heap property achieved
                break;
            }
-           T temp = container.get(fromPosition);
-           container.set(fromPosition, container.get(positionToBeSwapped));
-           container.set(positionToBeSwapped, temp);
-           fromPosition = positionToBeSwapped;
+           if (container.get(fromPosition).compareTo(container.get(positionToBeSwapped)) < 0) {
+               T temp = container.get(fromPosition);
+               container.set(fromPosition, container.get(positionToBeSwapped));
+               container.set(positionToBeSwapped, temp);
+               fromPosition = positionToBeSwapped;
+           } else {
+               // heap property achieved
+               break;
+           }
        }
+    }
+
+    private int getParent(int index) {
+        return ((index + 1) / 2) - 1;
+    }
+
+    private int getLeftChild(int index) {
+        return ((index + 1) * 2) - 1;
+    }
+
+    private int getRightChild(int index) {
+        return (((index + 1) * 2) + 1) - 1;
     }
 
 }
